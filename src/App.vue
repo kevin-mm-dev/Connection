ab055e49b5ef2730d6583cafd7bb6c2502342f79
kevin-mm-dev <kevin.mm.dev@gmail.com>
2019-09-18 17:29:10 -0500

    Proyecto listo para empezar

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  #app
+    img(src="./assets/logo.png")
+    h1 {{mensaje}}
+</template>
+
+<script>
+// import './assets/scss/main.scss'
+export default {
+  name: 'app',
+  data () {
+    return {
+      mensaje: 'Que pasa mi amigo!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss';
+</style>
