e94f3451e0750f90be30f356edfa644aa25d027f
kevin-mm-dev <kevin.mm.dev@gmail.com>
2019-09-18 19:58:45 -0500

    Header creado

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
-<template lang="pug">
-  #app
+<!--
     img(src="./assets/logo.png")
     h1 {{mensaje}}
+-->
+
+
+<template lang="pug">
+  #app
+    nav#principal
+      h1 Connection
+      button Log Out
+    nav#menuAdmin 
+      ul
+          li 
+            a Inicio
+          li  
+            a Reparaciones
+          li
+            a Reportes
+          li
+            a Usuarios
+    hr
+    section
+      h1.txtTitulo Agregar reparación
+      .fondo
+      hr#lnSubtitulo
 </template>
 
 <script>
@@ -10,7 +32,7 @@ export default {
   name: 'app',
   data () {
     return {
-      mensaje: 'Que pasa mi amigo!'
+      mensaje: 'Que pasa mi amigo'
     }
   }
 }
