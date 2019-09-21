60332ae506be8da88844cc9d0296fb0b37b0dc50
kevin-mm-dev <kevin.mm.dev@gmail.com>
2019-09-20 19:30:43 -0500

    Avance formulario vista de gerentes

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     h1 {{mensaje}}
 -->
 
-
 <template lang="pug">
   #app
     nav#principal
@@ -11,29 +10,55 @@
       button Log Out
     nav#menuAdmin 
       ul
-          li 
-            a Inicio
-          li  
-            a Reparaciones
-          li
-            a Reportes
-          li
-            a Usuarios
+        li 
+          a Inicio
+        li  
+          a Reparaciones
+        li
+          a Reportes
+        li
+          a Usuarios
     hr
-    section
-      h1.txtTitulo Agregar reparación
-      .fondo
-      hr#lnSubtitulo
+    etqAgregar
+    //- .columns
+    //-   .column.is-half A la mera mitad
+
 </template>
 
 <script>
 // import './assets/scss/main.scss'
+import jsPDF from 'jspdf'
+import subAgregar from './componentesVue/subAgregar.vue'
 export default {
   name: 'app',
   data () {
     return {
-      mensaje: 'Que pasa mi amigo'
+      mensaje: 'Que pasa mi amigo',
+      tablaImprimir:[
+        {titulo:'titulo 1', mensaje:'desc1'},
+        {titulo:'titulo 2', mensaje:'desc2'},
+      ]
     }
+  },
+  methods:{
+    saludar(){
+      alert("Esto es un metodo");
+    },
+    //  exportar(){
+       
+    //   const SELF=this;
+    //   var columnas=[
+    //     {titulo:"aquiTitulo", datakey:"titukey"},
+    //     {titulo:"mensajes", datakey:"menskey"}
+    //   ];
+    //   var doc=new jsPDF('p','pt');
+    //   doc.autoTable(columnas, SELF.tablaImprimir);
+    //   doc.save('aquitudoc.pdf');
+
+    // }
+  },
+  components:{
+    etqAgregar:subAgregar
   }
 }
 </script>
---
+++ src/componentesVue/subAgregar.vue
@@ -0,0 +1,173 @@
+
+<template lang="pug">
+  #subAgregar
+    .fondoTitulo
+    h1.txtTitulo Agregar Reparaciones
+    hr.txtTitulo
+    h2.txtSubTitulo Cliente
+    .marco.marco1
+      .bd-content(style ="width:100%;")
+        .columns
+          .column.is-half
+            .field
+              label.label Celular
+              .control
+                input.input.is-rounded(v-model="celular" name="cel" type="tel" placeholder="" )
+                p.help.is-danger(v-if="campoCompleto") Este campo es obligatorio
+          .column
+            .field
+              label.label Telefono de Casa
+              .control
+                input.input(v-model="telefono" name="tel" type="tel" placeholder="" )
+                p.help.is-danger(v-if="campoCompleto") Este campo es obligatorio
+        .columns
+          .column
+            .field
+              label.label Nombre
+              .control
+                input.input(v-model="nombre" name="name" type="text" placeholder="" )
+    br
+    br
+    h2.txtSubTitulo Equipo
+    .marco.marco2
+          .bd-content(style ="width:100%;")
+            .columns
+              .column.is-half
+                .field
+                  label.label Celular
+                  .control
+                    input.input.is-rounded(v-model="celular" name="cel" type="tel" placeholder="" )
+                    p.help.is-danger(v-if="campoCompleto") Este campo es obligatorio
+              .column
+                .field
+                  label.label Telefono de Casa
+                  .control
+                    input.input(v-model="telefono" name="tel" type="tel" placeholder="" )
+                    p.help.is-danger(v-if="campoCompleto") Este campo es obligatorio
+            .columns
+              .column
+                .field
+                  label.label Nombre
+                  .control
+                    input.input(v-model="nombre" name="name" type="text" placeholder="" )
+
+          //- .control
+            button.button(@click="validar" type="submit") valer
+            h1
+
+
+          //- form
+          //-   label(for="celular") Celular:
+          //-   input.datosCliente(name="celular" type="text" placeholder="000-000-00-00" )
+          //-   label(for="name") Nombre:
+          //-   input.lblNombre(name="name" type="password" placeholder="")
+
+            //- .column.is-half
+            //-   h3(v-model="cel") Nombre:
+
+
+</template>
+
+<script>
+// import prueba from '../../'
+export default {
+  name: 'subAgregar',
+  props:[],
+  data (){
+    return {
+      nombre:"",
+      celular:"",
+      campoCompleto:false
+    }
+  },
+  methods:{
+    validar(){
+      if (this.campoCompleto) {
+        this.campoCompleto=false
+      }else{
+        this.campoCompleto=true
+      }
+    }
+  },
+  components:{},
+
+}
+</script>
+
+<style lang="scss">
+@import '../scss/main.scss';
+// @import '../../node_modules/bulma/bulma.sass';
+.txtTitulo {
+    color: $fondoBarra;
+    font-size: 2.4rem;
+    margin-left: 10px;
+    position: absolute;
+    top:110px;
+}
+
+.fondoTitulo {
+    position: absolute;
+    width: 280px;
+    height: 39px;
+    left: -34px;
+    top: 135px;
+    background: #C6FFCB;
+    filter: blur(9px);
+}
+hr.txtTitulo {
+    height: .2rem;
+    background: #4D4A4A;
+    margin: 0%;
+    width: 290px;
+    top: 11rem;
+}
+
+
+
+
+//////////////////////////  EXTRAS
+.marco{
+    border-radius: 4px;
+    border: solid;
+}
+.txtSubTitulo{
+  margin-left: 215px;
+    top: 13rem;
+    position: absolute;
+    background: #a7ff8a;
+    padding: 10px;
+    font-size: 20px;
+    font-family: sans-serif;
+    border: solid;
+    border-radius: 5px;
+}
+.marco1{
+    height: 14rem;
+    width: 45rem;
+    margin-left: 10rem;
+    margin-top: 8rem;
+    padding: 25px;
+}
+.marco2{
+    height: 34rem;
+    width: 45rem;
+    margin-left: 10rem;
+    padding: 25px;
+    
+}
+input.nombre{
+width: 25rem;
+}
+// .field{
+//   margin-right: 50px;
+// }
+.lblNombre{
+  width:20rem;
+}
+
+
+// rem o px
+// imprimir PDF
+// como obtener el id de un elemento en la tabla
+
+</style>
